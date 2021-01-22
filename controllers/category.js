3a667103076d03b574f37ec1649e73759a46850c
emmanueltech2019 <57847579+emmanueltech2019@users.noreply.github.com>
2021-01-22 06:27:15 +0100

    done

--- controllers/category.js
+++ controllers/category.js
@@ -1,55 +1,69 @@
-const slugify = require('slugify')
-const Category = require('../models/Category')
+const slugify = require("slugify");
+const Category = require("../models/Category");
+const Products = require("../models/Product");
 
+const createCategories = (categories, parentId = null) => {
+  const categoryList = [];
+  let category;
+  if (parentId == null) {
+    category = categories.filter((cat) => cat.parentId == undefined);
+  } else {
+    category = categories.filter((cat) => cat.parentId == parentId);
+  }
+  for (let cate of category) {
+    categoryList.push({
+      _id: cate._id,
+      name: cate.name,
+      slug: cate.slug,
+      children: createCategories(categories, cate._id),
+    });
+  }
+  return categoryList;
+};
+exports.createCategory = (req, res) => {
+  const { name, parentId } = req.body;
+  const categoryObj = {
+    name: name,
+    slug: slugify(name),
+  };
+  if (parentId) {
+    categoryObj.parentId = parentId;
+  }
 
-const createCategories=(categories,parentId=null)=>{
-    const categoryList =[]
-    let category;
-    if(parentId==null){
-        category = categories.filter(cat=>cat.parentId==undefined)
+  const cat = new Category(categoryObj);
+  cat.save((error, category) => {
+    if (error) {
+      res.status(400).json({ error });
     }
-    else{
-        category = categories.filter(cat=>cat.parentId==parentId)
+    if (category) {
+      res
+        .status(201)
+        .json({ category, message: `${name} Category successfully created` });
     }
-    for(let cate of category){
-        categoryList.push({
-            _id:cate._id,
-            name:cate.name,
-            slug:cate.slug,
-            children:createCategories(categories,cate._id)
-        })
-    }
-    return categoryList
-}
-exports.createCategory=(req,res)=>{
-    const {name,parentId} =req.body
-    const categoryObj ={
-        name:name,
-        slug:slugify(name)
-    }
-    if(parentId){
-        categoryObj.parentId=parentId
-    }
+  });
+};
 
-    const cat = new Category(categoryObj)
-    cat.save((error,category)=>{
-        if (error) {
-            res.status(400).json({error})
+exports.getCategories = (req, res) => {
+  Category.find({}).exec((error, categories) => {
+    if (error) return res.status(400).json({ error });
+    if (categories) {
+      const categoryList = createCategories(categories);
+      res.status(200).json({ categoryList });
+    }
+  });
+};
+
+exports.singleCategoryProducts = (req, res) => {
+  Category.findOne({ _id: req.params.id }, (err, category) => {
+    if (err) res.status(400).json({ error: err });
+    if (category) {
+      Products.find({ category: category._id }, (err, products) => {
+        if (err) res.status(400).json({ error: err });
+        if (products) {
+          res.status(200).json({ products });
         }
-        if(category){
-            res.status(201).json({category,message:`${name} Category successfully created`})
-        }
-    })
-}
-
-
-exports.getCategories=(req,res)=>{
-    Category.find({})
-    .exec((error,categories)=>{
-        if(error) return res.status(400).json({error})
-        if(categories){
-            const categoryList = createCategories(categories)
-            res.status(200).json({categoryList})
-        }
-    })
-}
+      });
+    }
+    // res.send(category)
+  });
+};
--- routes/Category/index.js
+++ routes/Category/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { createCategory,getCategories } = require('../../controllers/category')
+const { createCategory,getCategories, singleCategoryProducts } = require('../../controllers/category')
 const { requireSignin, adminMiddleware } = require('../../common-middlewares/')
 const router =express.Router()
 
 
 router.post('/category/create',requireSignin,adminMiddleware,createCategory)
 router.get('/category/getCategory',getCategories)
+router.get('/category/singleCategoryProducts/:id',singleCategoryProducts)
 
 module.exports =router
