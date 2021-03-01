99cdd04f4bcb02974c5130916009579813f2994c
emmanueltech2019 <57847579+emmanueltech2019@users.noreply.github.com>
2021-03-01 11:31:17 +0100

    done

--- routes/order/index.js
+++ routes/order/index.js
@@ -1,5 +1,6 @@
 const express =require('express')
 const { placeOrder } = require('../../controllers/order')
+const { userMiddleware,requireSignin } = require('../../common-middlewares')
 const router =express.Router()
 
 router.post('/order/add',requireSignin,userMiddleware,placeOrder)
