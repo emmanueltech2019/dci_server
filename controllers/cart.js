30490aeba872abffa43855140197821f91c5fd7f
emmanueltech2019 <57847579+emmanueltech2019@users.noreply.github.com>
2021-03-02 07:11:45 +0100

    done

--- controllers/cart.js
+++ controllers/cart.js
@@ -176,7 +176,7 @@ exports.getCartItems = (req, res) => {
               img: item.product.productPictures[0]?item.product.productPictures[0].img:null,
               price: item.product.price,
               qty: item.quantity, 
-              size: item.size, 
+              size: item.product.size, 
             };
           }
         });
